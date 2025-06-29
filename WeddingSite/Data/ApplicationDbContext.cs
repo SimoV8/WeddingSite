8d4b9d28895b2c6249dc6110a3dc2d47401c30f6
Simone Vuotto <simov8@gmail.com>
2025-06-29 22:39:13 +0200

    Add message page

--- WeddingSite/Data/ApplicationDbContext.cs
+++ WeddingSite/Data/ApplicationDbContext.cs
@@ -5,5 +5,22 @@ namespace WeddingSite.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+
+        static ApplicationDbContext()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public DbSet<WeddingMessage> WeddingMessages { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<WeddingMessage>()
+                .HasOne(m => m.User)
+                .WithMany()
+                .HasForeignKey(m => m.UserId);
+        }
     }
 }
